cdf9d9d48f5b924ec7190a4dd5ccabdf771c407b
JimsonR <jimsonjimmy2008@gmail.com>
2025-03-29 00:11:40 +0530

    kube deploys changes

---
+++ configuration/iaac/kubernetes/variables.tf
@@ -0,0 +1,34 @@
+variable client_id {
+  
+}
+variable client_secret {
+  
+}
+variable ssh_public_key {
+  
+}
+
+variable environment {
+  default = "dev"
+}
+
+variable location {
+    default = "westeurope"
+}
+
+variable node_count {
+  default = 3
+}
+
+variable dns_prefix {
+  default = "prodtest"
+}
+
+variable cluster_name {
+    default = "prodtest"
+
+}
+
+variable resource_group {
+  default = "kubernetes"
+}
