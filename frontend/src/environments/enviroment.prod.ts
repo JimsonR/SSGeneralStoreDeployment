74dbf4fc9c583381e9488e756ce9aed53886711f
JimsonR <jimsonjimmy2008@gmail.com>
2025-03-31 15:42:34 +0530

    pipeline name change frontend-service to frontend

--- frontend/src/environments/enviroment.prod.ts
+++ frontend/src/environments/enviroment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
     production : true,
-    api: "https://prodtest-dev-123456.hcp.southcentralus.azmk8s.io/api"
+    // api: "https://prodtest-dev-123456.hcp.southcentralus.azmk8s.io/api"
+    // api: 'http://ssgenerals-backend.default.svc.cluster.local:8080/api'
+    api: '/api'
+
 }
