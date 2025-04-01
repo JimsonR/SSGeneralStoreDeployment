b04961260d4a5530e55b8ff34bc4765f388c4d69
JimsonR <jimsonjimmy2008@gmail.com>
2025-04-01 15:33:57 +0530

    url changed

--- backend/SuryaSankaraBackend/src/main/java/com/example/SuryaSankaraBackend/controller/HealthEndpoint.java
+++ backend/SuryaSankaraBackend/src/main/java/com/example/SuryaSankaraBackend/controller/HealthEndpoint.java
@@ -1,22 +1,22 @@
-package com.example.SuryaSankaraBackend.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/actuator/health")
-public class HealthEndpoint {
-    @GetMapping("/")
-    public String imHealthy() {
-        return "{healthy:true}";
-    }
-    @GetMapping("/readiness")
-    public String imReady() {
-        return "{healthy:true}";
-    }
-    @GetMapping("/liveness")
-    public String imLive() {
-        return "{healthy:true}";
-    }
-}
+//package com.example.SuryaSankaraBackend.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/actuator/health")
+//public class HealthEndpoint {
+//    @GetMapping("/")
+//    public String imHealthy() {
+//        return "{healthy:true}";
+//    }
+//    @GetMapping("/readiness")
+//    public String imReady() {
+//        return "{healthy:true}";
+//    }
+//    @GetMapping("/liveness")
+//    public String imLive() {
+//        return "{healthy:true}";
+//    }
+//}
